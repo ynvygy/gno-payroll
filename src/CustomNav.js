56977c4de76b8b8ad60b37978baeebd1f1002c4a
ynvygy <alexandru_musat@yahoo.com>
2023-05-28 17:13:27 +0300

    reorder navbar

--- src/CustomNav.js
+++ src/CustomNav.js
@@ -46,12 +46,12 @@ const CustomNav = ({ account, setAccount, connectWallet, disconnectHandler, acco
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="flex-column">
               <Nav.Link href="/" className={selectedLink === '/' ? 'selected' : ''} onClick={() => handleLinkClick('/')}>Dashgnord</Nav.Link>
+              <Nav.Link href="/gnowner" className={selectedLink === '/gnowner' ? 'selected' : ''} onClick={() => handleLinkClick('/gnowner')}>Gnowner</Nav.Link>
               <Nav.Link href="/employees" className={selectedLink === '/employees' ? 'selected' : ''} onClick={() => handleLinkClick('/employees')}>Employees</Nav.Link>
               <Nav.Link href="/addemployee" className={selectedLink === '/addemployee' ? 'selected' : ''} onClick={() => handleLinkClick('/addemployee')}>Add Employee</Nav.Link>
               <Nav.Link href="/taxrates" className={selectedLink === '/taxrates' ? 'selected' : ''} onClick={() => handleLinkClick('/taxrates')}>Tax Rates</Nav.Link>
               <Nav.Link href="/addtaxrates" className={selectedLink === '/addtaxrates' ? 'selected' : ''} onClick={() => handleLinkClick('/addtaxrates')}>Tax builder</Nav.Link>
               <Nav.Link href="/hoursreport" className={selectedLink === '/hoursreport' ? 'selected' : ''} onClick={() => handleLinkClick('/hoursreport')}>Timesheet</Nav.Link>
-              <Nav.Link href="/gnowner" className={selectedLink === '/gnowner' ? 'selected' : ''} onClick={() => handleLinkClick('/gnowner')}>Gnowner</Nav.Link>
               <Nav.Link href="/employeereport" className={selectedLink === '/employeereport' ? 'selected' : ''} onClick={() => handleLinkClick('/employeereport')}>Employee Report</Nav.Link>
               <Nav.Link href="/gnownerreport" className={selectedLink === '/gnownerreport' ? 'selected' : ''} onClick={() => handleLinkClick('/gnownerreport')}>Gnowner Report</Nav.Link>
             </Nav>
